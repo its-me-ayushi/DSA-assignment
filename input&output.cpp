943614f2b48dfc4706607eb66008e60dfb2dc6e0
ayushi adya <105603159+its-me-ayushi@users.noreply.github.com>
2022-09-21 20:32:49 +0530

    Create input&output.cpp

---
+++ input&output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,m,c;
+    cin>>n>>m>>c;
+    int sum = n+m+c;
+    cout<<sum;
+    return 0;
+}
+
